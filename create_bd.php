df149dceff9170e94c30bd90c76b57b8c5ddb908
Kostyk163 <46649456+Kostyk163@users.noreply.github.com>
2019-02-07 16:03:11 +0300

    Add files via upload

---
+++ create_bd.php
@@ -0,0 +1,15 @@
+<?php
+$root = "root";
+$root_password = "";
+$host = "localhost";
+
+$user = 'root';
+$pass = '';
+$db = "testaltarix";
+
+try {
+    $dbh = new PDO("mysql:host=$host", $root, $root_password);
+    $dbh->exec("CREATE DATABASE `$db`;");
+} catch (PDOException $e) {
+    die("DB ERROR: " . $e->getMessage());
+}
