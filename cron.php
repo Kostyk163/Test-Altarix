98d2e67cd87749a529775d316f82a4180b4e23dc
Kostyk163 <46649456+Kostyk163@users.noreply.github.com>
2019-02-08 19:22:00 +0300

    Update cron.php

--- cron.php
+++ cron.php
@@ -4,16 +4,16 @@ $url = "http://dev.soap-task.blackhole.marm.altarix.org/wsdl";
 
 $client = new SoapClient($url, array('trace' => 1));
 
-$result = $client->getTaxiInfo(array("regNum"=> "ЕМ333777"));
+$result    = $client->getTaxiInfo(array("regNum"=> "ЕМ333777"));
 $resultate = $result->getTaxiInfoResult;
 print_r($resultate);
 echo $client->__getLastResponse() . " ss " . $client->__getLastRequestHeaders();
 //print_r($response);
 
-$res = implode(" , ", $resultate);
+$res = implode(" ", $resultate);
 
-$ok = 'OK';
-$fail = 'FAIL';
+$ok     = 'OK';
+$fail   = 'FAIL';
 $params = [
     'licenseNum'  => '02651',
     'licenseDate' => '2011-08-08T00:00:00+00:00',
@@ -27,34 +27,35 @@ $params = [
     'blankNum'    => '002695',
 ];
 
-$str_params = implode(" , ", $params);
+$str_params = implode(" ", $params);
 
-if ($str_params == $res){
-    echo $ok;
-}else {
-        echo $fail;
+if ($str_params == $res) {
+    echo $ok = $status;
+}
+else {
+    echo $fail = $status;
+
 }
 
-$id = '';
-$time_request = '';
-$time_response = '';
-$time_vait = $time_response - $time_request;
-$status = '';
+$id            = is_integer($id);
+$time_request  = date("d.m.Y H:m:s");
+$time_response = date("d.m.Y H:m:s");
+$time_wait     = $time_response - $time_request;
+$status        = '';
 $body_response = $client->__getLastResponse;
 
 $host = "localhost";
 $user = 'root';
 $pass = '';
-$db = "testaltarix";
+$db   = "testaltarix";
 
 $link = mysqli_connect("$host" , "$user" , "$pass" , "$db");
 if ( !$link ) {
     echo 'Ошибка: ' . mysqli_connect_errno() . ':' . mysqli_connect_error();
 }
 
-$sql = "SELECT * FROM users
-        INSERT INTO users(id, time_request, time_response, time_vait, status, body_response)
-        VALUES ($id, $time_request, $time_response, $time_vait, $status, $body_response)";
+$sql = "INSERT INTO list(id, time_request, time_response, time_wait, status, body_response)
+        VALUES ($id, $time_request, $time_response, $time_wait, $status, $body_response)";
 if (mysqli_query($link, $sql)) {
     echo "Запись добавлена";
 } else {
