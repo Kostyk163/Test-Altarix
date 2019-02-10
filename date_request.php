4ba05854cbf2327a1f5d77a9119d21706b51a001
Kostyk163 <46649456+Kostyk163@users.noreply.github.com>
2019-02-11 01:14:00 +0300

    Add files via upload

---
+++ date_request.php
@@ -0,0 +1,17 @@
+<?php
+$root          = "root";
+$root_password = "";
+$host          = "localhost";
+
+$user = 'root';
+$pass = '';
+$db   = "testaltarix";
+$sql = "";
+
+$dbh = new PDO("mysql:dbname=$db;host=$host", $root, $root_password);
+$result = $dbh->query("SELECT `time_request` FROM `list`")->fetchAll(PDO::FETCH_COLUMN);
+
+$result = implode(",", $result);
+//$result = date('d-m-Y H:i:s', strtotime($result));
+//$result = strtotime($result);
+echo $result;
